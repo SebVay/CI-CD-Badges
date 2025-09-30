eab1a2f939a1faa0b5c4d2e5085fc5f42ff7507e
Sébastien <sebast.mar@gmail.com>
2025-09-30 18:50:56 +0200

    Add missing html report

--- ci/config.sh
+++ ci/config.sh
@@ -1,7 +1,9 @@
 # Urls
-export GITHUB_REPOSITORY_URL="https://github.com/SebVay/CI-CD-Badges"
-export GITHUB_PAGE_URL="https://sebvay.github.io/CI-CD-Badges"
-export GITHUB_RAW_REPOSITORY_URL="https://raw.githubusercontent.com/SebVay/CI-CD-Badges"
+export GROUP_PAGE="SebVay"
+export GITHUB_GROUP_PAGE_URL="https://github.com/$GROUP_PAGE"
+export GITHUB_REPOSITORY_URL="$GITHUB_GROUP_PAGE_URL/CI-CD-Badges"
+export GITHUB_PAGE_URL="https://$GROUP_PAGE.github.io/CI-CD-Badges"
+export GITHUB_RAW_REPOSITORY_URL="https://raw.githubusercontent.com/$GROUP_PAGE/CI-CD-Badges"
 
 # Badges
 export COVERAGE_BADGE_JSON="coverage.json"
